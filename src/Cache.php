41bc92c3d89c3cda7a514063424beeb21cce1d1b
alex <766f6964+github@gmail.com>
2025-11-07 14:19:51 +0100

    \Psr\SimpleCache\CacheInterface implementation

--- src/Cache.php
+++ src/Cache.php
@@ -2,50 +2,46 @@
 
 namespace aportela\SimpleFSCache;
 
-class Cache
+class Cache implements \Psr\SimpleCache\CacheInterface
 {
     protected \Psr\Log\LoggerInterface $logger;
 
-    private ?string $basePath = null;
-    private bool $enabled = true;
+    private string $basePath;
     private \aportela\SimpleFSCache\CacheFormat $format;
-    private bool $ignoreExistingCache = false;
 
-    public function __construct(\Psr\Log\LoggerInterface $logger, \aportela\SimpleFSCache\CacheFormat $format = \aportela\SimpleFSCache\CacheFormat::NONE, ?string $basePath = null, bool $ignoreExistingCache = false)
+    public function __construct(\Psr\Log\LoggerInterface $logger, string $basePath, \aportela\SimpleFSCache\CacheFormat $format = \aportela\SimpleFSCache\CacheFormat::NONE)
     {
         $this->logger = $logger;
         $this->setBasePath($basePath);
         $this->format = $format;
-        $this->ignoreExistingCache = $ignoreExistingCache;
     }
 
-    public function __destruct()
-    {
-    }
+    public function __destruct() {}
 
-    public function isEnabled(): bool
-    {
-        return ($this->enabled);
-    }
-
-    public function setBasePath(?string $basePath = null): void
+    public function setBasePath(string $basePath): void
     {
         if (! empty($basePath)) {
             if (!file_exists(($basePath))) {
                 $this->logger->info("\aportela\SimpleFSCache\Cache::setBasePath - Creating missing path: {$basePath}");
                 if (! mkdir($basePath, 0750)) {
-                    $this->logger->error("\aportela\SimpleFSCache\Cache::__construct - Error creating missing path: {$basePath}");
-                    throw new \aportela\SimpleFSCache\Exception\FileSystemException("Error creating missing path: {$basePath}");
+                    $this->logger->error("\aportela\SimpleFSCache\Cache::__construct - Error while creating missing path: {$basePath}");
+                    throw new \aportela\SimpleFSCache\Exception\CacheException("Error while creating missing path: {$basePath}");
                 }
             }
-            $this->basePath = ($basePath = realpath($basePath)) ? $basePath : null;
+            $path = realpath($basePath);
+            if (is_string($path) && ! empty($path)) {
+                $this->basePath = $path;
+            } else {
+                $this->logger->error("\aportela\SimpleFSCache\Cache::setBasePath - Error: invalid path {$basePath}");
+                throw new \aportela\SimpleFSCache\Exception\InvalidArgumentException("invalid path {$basePath}");
+            }
         } else {
-            $this->logger->info("\aportela\SimpleFSCache\Cache::setBasePath - empty value");
+            $this->logger->error("\aportela\SimpleFSCache\Cache::setBasePath - empty path");
+            throw new \aportela\SimpleFSCache\Exception\InvalidArgumentException("empty path");
         }
-        $this->enabled = ! empty($this->basePath);
     }
 
-    public function getBasePath(): null|string
+    public function getBasePath(): string
     {
         return ($this->basePath);
     }
@@ -63,110 +59,211 @@ class Cache
     /**
      * return cache directory path
      */
-    public function getCacheDirectoryPath(string $identifier): string
+    public function getCacheDirectoryPath(string $key): string
     {
-        return ($this->basePath . DIRECTORY_SEPARATOR . mb_substr($identifier, 0, 1) . DIRECTORY_SEPARATOR . mb_substr($identifier, 1, 1) . DIRECTORY_SEPARATOR . mb_substr($identifier, 2, 1) . DIRECTORY_SEPARATOR . mb_substr($identifier, 3, 1));
+        return ($this->basePath . DIRECTORY_SEPARATOR . mb_substr($key, 0, 1) . DIRECTORY_SEPARATOR . mb_substr($key, 1, 1) . DIRECTORY_SEPARATOR . mb_substr($key, 2, 1) . DIRECTORY_SEPARATOR . mb_substr($key, 3, 1));
     }
 
     /**
      * return cache file path
      */
-    public function getCacheFilePath(string $identifier): string
+    public function getCacheFilePath(string $key): string
     {
-        $basePath = $this->getCacheDirectoryPath($identifier);
+        $basePath = $this->getCacheDirectoryPath($key);
         if ($this->format !== \aportela\SimpleFSCache\CacheFormat::NONE) {
-            return (sprintf("%s%s%s.%s", $basePath, DIRECTORY_SEPARATOR, $identifier, $this->format->value));
+            return (sprintf("%s%s%s.%s", $basePath, DIRECTORY_SEPARATOR, $key, $this->format->value));
         } else {
-            return ($basePath . DIRECTORY_SEPARATOR . $identifier);
+            return ($basePath . DIRECTORY_SEPARATOR . $key);
         }
     }
 
     /**
-     * save current raw data into disk cache
+     * Fetches a value from the cache.
+     *
+     * @param string $key     The unique key of this item in the cache.
+     * @param mixed  $default Default value to return if the key does not exist.
+     *
+     * @return mixed The value of the item from the cache, or $default in case of cache miss.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
      */
-    public function save(string $identifier, string $raw): bool
+    public function get(string $key, mixed $default = null): mixed
     {
-        if ($this->enabled) {
-            try {
-                if (! empty(mb_trim($raw))) {
-                    $directoryPath = $this->getCacheDirectoryPath($identifier);
-                    if (! file_exists($directoryPath)) {
-                        if (!mkdir($directoryPath, 0750, true)) {
-                            $this->logger->error("\aportela\SimpleFSCache\Cache::save - Error creating cache file path", [$identifier, $directoryPath]);
-                            throw new \aportela\SimpleFSCache\Exception\FileSystemException("Error creating cache file path: {$directoryPath}");
-                        }
-                    }
-                    return (file_put_contents($this->getCacheFilePath($identifier), $raw, LOCK_EX) > 0);
-                } else {
-                    $this->logger->info("\aportela\SimpleFSCache\Cache::save - Cache value is empty, saving ignored", [$identifier]);
-                    return (false);
-                }
-            } catch (\Throwable $e) {
-                $this->logger->error("\aportela\SimpleFSCache\Cache::save - Error saving cache", [$identifier, $e->getMessage(), $e->getCode()]);
-                return (false);
-            }
-        } else {
-            return (false);
+        if (empty($key)) {
+            throw new \Psr\SimpleCache\InvalidArgumentException("invalid cache key");
         }
-    }
 
-    /**
-     * remove cache entry
-     */
-    public function remove(string $identifier): bool
-    {
-        if ($this->enabled) {
-            try {
-                $cacheFilePath = $this->getCacheFilePath($identifier);
-                if (file_exists($cacheFilePath)) {
-                    if (unlink($cacheFilePath)) {
-                        return (true);
-                    } else {
-                        $this->logger->error("\aportela\SimpleFSCache\Cache::remove - Error removing cache file", [$identifier, $cacheFilePath]);
-                        return (false);
-                    }
-                } else {
-                    $this->logger->info("\aportela\SimpleFSCache\Cache::remove - Cache file not found, can not remove", [$identifier, $cacheFilePath]);
-                    return (false);
-                }
-            } catch (\Throwable $e) {
-                $this->logger->error("Error removing cache", [$identifier, $e->getMessage()]);
-                return (false);
-            }
-        } else {
-            return (false);
-        }
-    }
-
-    /**
-     * read disk cache
-     */
-    public function get(string $identifier): bool|string
-    {
-        if ($this->enabled && ! $this->ignoreExistingCache) {
-            try {
-                $cacheFilePath = $this->getCacheFilePath($identifier);
-                if (file_exists($cacheFilePath)) {
+        try {
+            $cacheFilePath = $this->getCacheFilePath($key);
+            if (file_exists($cacheFilePath)) {
+                $time = filemtime($cacheFilePath);
+                if (is_int($time)) { // unix timestamp
                     return (file_get_contents($cacheFilePath));
                 } else {
-                    $this->logger->debug("\aportela\SimpleFSCache\Cache::get - Cache file not found", [$identifier, $cacheFilePath]);
-                    return (false);
+                    // TODO
+                    return ($default);
                 }
-            } catch (\Throwable $e) {
-                $this->logger->error("\aportela\SimpleFSCache\Cache::get - Error loading cache file", [$identifier, $e->getMessage(), $e->getCode()]);
+            } else {
+                $this->logger->debug("\aportela\SimpleFSCache\Cache::get - Cache file not found", [$key, $cacheFilePath]);
+                return ($default);
+            }
+        } catch (\Throwable $e) {
+            $this->logger->error("\aportela\SimpleFSCache\Cache::get - Error loading cache file", [$key, $e->getMessage(), $e->getCode()]);
+            // TODO
+            return ($default);
+        }
+    }
+
+    /**
+     * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
+     *
+     * @param string                 $key   The key of the item to store.
+     * @param mixed                  $value The value of the item to store, must be serializable.
+     * @param null|int|\DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
+     *                                      the driver supports TTL then the library may set a default value
+     *                                      for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null): bool
+    {
+        try {
+            if (! empty(mb_trim($value))) {
+                $directoryPath = $this->getCacheDirectoryPath($key);
+                if (! file_exists($directoryPath)) {
+                    if (!mkdir($directoryPath, 0750, true)) {
+                        $this->logger->error("\aportela\SimpleFSCache\Cache::save - Error creating cache file path", [$key, $directoryPath]);
+                        throw new \aportela\SimpleFSCache\Exception\FileSystemException("Error creating cache file path: {$directoryPath}");
+                    }
+                }
+                return (file_put_contents($this->getCacheFilePath($key), $value, LOCK_EX) > 0);
+            } else {
+                $this->logger->info("\aportela\SimpleFSCache\Cache::save - Cache value is empty, saving ignored", [$key]);
                 return (false);
             }
-        } else {
+        } catch (\Throwable $e) {
+            $this->logger->error("\aportela\SimpleFSCache\Cache::save - Error saving cache", [$key, $e->getMessage(), $e->getCode()]);
             return (false);
         }
     }
 
-    public function isCached(string $identifier): bool
+    /**
+     * Delete an item from the cache by its unique key.
+     *
+     * @param string $key The unique cache key of the item to delete.
+     *
+     * @return bool True if the item was successfully removed. False if there was an error.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function delete(string $key): bool
     {
-        if ($this->enabled && ! $this->ignoreExistingCache) {
-            return (file_exists($this->getCacheFilePath($identifier)));
-        } else {
+        try {
+            $cacheFilePath = $this->getCacheFilePath($key);
+            if (file_exists($cacheFilePath)) {
+                if (unlink($cacheFilePath)) {
+                    return (true);
+                } else {
+                    $this->logger->error("\aportela\SimpleFSCache\Cache::remove - Error removing cache file", [$key, $cacheFilePath]);
+                    return (false);
+                }
+            } else {
+                $this->logger->info("\aportela\SimpleFSCache\Cache::remove - Cache file not found, can not remove", [$key, $cacheFilePath]);
+                return (false);
+            }
+        } catch (\Throwable $e) {
+            $this->logger->error("Error removing cache", [$key, $e->getMessage()]);
             return (false);
         }
+    }
+
+    /**
+     * Wipes clean the entire cache's keys.
+     *
+     * @return bool True on success and false on failure.
+     */
+    public function clear(): bool
+    {
+        return (false);
+    }
+
+    /**
+     * Obtains multiple cache items by their unique keys.
+     *
+     * @param iterable<string> $keys    A list of keys that can be obtained in a single operation.
+     * @param mixed            $default Default value to return for keys that do not exist.
+     *
+     * @return iterable<string, mixed> A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if $keys is neither an array nor a Traversable,
+     *   or if any of the $keys are not a legal value.
+     */
+    public function getMultiple(iterable $keys, mixed $default = null): iterable
+    {
+        return ([]);
+    }
+
+    /**
+     * Persists a set of key => value pairs in the cache, with an optional TTL.
+     *
+     * @param iterable               $values A list of key => value pairs for a multiple-set operation.
+     * @param null|int|\DateInterval $ttl    Optional. The TTL value of this item. If no value is sent and
+     *                                       the driver supports TTL then the library may set a default value
+     *                                       for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if $values is neither an array nor a Traversable,
+     *   or if any of the $values are not a legal value.
+     */
+    public function setMultiple(iterable $values, null|int|\DateInterval $ttl = null): bool
+    {
+        return (false);
+    }
+
+    /**
+     * Deletes multiple cache items in a single operation.
+     *
+     * @param iterable<string> $keys A list of string-based keys to be deleted.
+     *
+     * @return bool True if the items were successfully removed. False if there was an error.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if $keys is neither an array nor a Traversable,
+     *   or if any of the $keys are not a legal value.
+     */
+    public function deleteMultiple(iterable $keys): bool
+    {
+        foreach ($keys as $key) {
+            $this->delete($key);
+        }
+        return (true);
+    }
+
+    /**
+     * Determines whether an item is present in the cache.
+     *
+     * NOTE: It is recommended that has() is only to be used for cache warming type purposes
+     * and not to be used within your live applications operations for get/set, as this method
+     * is subject to a race condition where your has() will return true and immediately after,
+     * another script can remove it making the state of your app out of date.
+     *
+     * @param string $key The cache item key.
+     *
+     * @return bool
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function has(string $key): bool
+    {
+        return (file_exists($this->getCacheFilePath($key)));
     }
 }
