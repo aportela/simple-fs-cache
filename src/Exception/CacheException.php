aa4e4ee38b0d142888b7c4179a0c26a11bc3375a
alex <766f6964+github@gmail.com>
2025-11-07 15:29:29 +0100

    php-cs-fixer changes

--- src/Exception/CacheException.php
+++ src/Exception/CacheException.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace aportela\SimpleFSCache\Exception;
 
-final class CacheException extends \Exception implements \Psr\SimpleCache\CacheException {}
+final class CacheException extends \Exception implements \Psr\SimpleCache\CacheException
+{
+}
--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace aportela\SimpleFSCache\Exception;
 
-final class InvalidArgumentException extends \Exception implements \Psr\SimpleCache\InvalidArgumentException {}
+final class InvalidArgumentException extends \Exception implements \Psr\SimpleCache\InvalidArgumentException
+{
+}
